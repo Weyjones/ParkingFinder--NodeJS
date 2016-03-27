1957c40396506b0f0b200e5ae8203ca646546cf6
Zhou <zhouyuli@amazon.com>
2016-03-27 16:28:28 -0700

    add apis for parking create and list

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var logger = require('./lib/modules/logger/logger');
+var utils = require('./lib/modules/utils/utils');
+
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false});
+
 var redis = require('./lib/modules/redis/redis');
 
 var app = express();
 app.use(logger);
+
 var fs = require('fs');
 
 app.use(express.static('public'));
 app.get('/listUsers', function(req, res) {
 	fs.readFile(__dirname + "/" + "users.json", "utf8", function(err, data) {
-		console.log(data);
 		res.send(data);
 	});
 });
@@ -20,13 +25,32 @@ app.get('/listStates', function(req, res) {
 	});
 });
 
-/*
-app.post('/parking/create', function(req, res) {
-	console.log(req.body);
-	//redis.lpush('parkings', )
-	res.send(req.body);
+/**
+ * parkingObject: {
+ * 		street: [string],
+ * 		city: [string],
+ * 		state: [string],
+ *		zip: [number],
+ *		longitude: [number],
+ *		latitude: [number],	
+ *		rate: [number],
+ *		type: [string]
+ * }
+ */
+app.post('/parking/create', parseUrlencoded, function(req, res) {
+	var parkingObject = req.body;
+	parkingObject.id = utils.uuid();
+	redis.lpush('parkings', JSON.stringify(parkingObject));
+	res.status(201).json(parkingObject);
 });
-*/
+
+app.get('/parking/list', function(req, res) {
+	redis.lrange('parkings', 0, -1, function(err, data) {
+		data = data.map(JSON.parse);
+		res.json(200, data);
+	});
+});
+
 app.get('/blocks', function(req, res) {
 	var blocks = ['Fixed', 'Movable', 'Rotating'];
 	res.send(blocks);
